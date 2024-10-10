72d2ffbe027c33fab570772d780dc9b080f1596d
Samruddhi Nadgouda <samusnadgouda2003@gmail.com>
2024-10-10 17:41:22 +0530

    commiting project to git

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
+
+const connectDB = async() =>{
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+        process.exit();
+    }
+};
+
+module.exports = connectDB;
---
+++ backend/server.js
@@ -0,0 +1,88 @@
+const express = require("express");
+require('dotenv').config();
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const { errorHandler, notFound } = require('./middleware/errorMiddleware');
+const path = require("path");
+const http = require("http");
+const socketIo = require("socket.io");
+
+// Connect to the database
+connectDB();
+
+const app = express();
+
+app.use(express.json()); // To accept JSON data
+
+app.use('/api/user', userRoutes);
+app.use('/api/chats', chatRoutes);
+app.use('/api/message', messageRoutes);
+
+// Deployment setup
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+// Create HTTP server
+const server = http.createServer(app);
+
+// Setup socket.io
+const io = socketIo(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+  
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message received", newMessageRecieved);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server Started on PORT ${PORT}`));
